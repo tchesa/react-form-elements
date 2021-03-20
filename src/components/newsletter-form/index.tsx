9e195e0d56f0c96adb42a5a59eddccff7a41b1b9
cesarlima-hotmart <cesar.lima@hotmart.com>
2021-03-20 19:19:07 -0300

    apply yup resolver

--- src/components/newsletter-form/index.tsx
+++ src/components/newsletter-form/index.tsx
@@ -1,19 +1,35 @@
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 import Form, { Field } from '../form'
 import Button from '../button'
 
-const NewsletterForm = () => {
-  const { register, handleSubmit  } = useForm()
+type User = {
+  name: string,
+  email: string
+}
 
-  const newUser = (user) => {
+const schema: yup.SchemaOf<User> = yup.object().shape({
+  name: yup.string().min(2).required(),
+  email: yup.string().email().required()
+})
+
+const NewsletterForm = () => {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const newUser = (user: User) => {
     console.log(user)
   }
 
   return (
-    <Form onSubmit={handleSubmit(newUser)}>
-      <Field.Input label="name" name="name" ref={register} />
-      <Field.Input label="name" name="email" type="email" ref={register} />
+    <Form onSubmit={handleSubmit<User>(newUser)}>
+      <Field.Input label="Name" name="name" ref={register} />
+      {errors.name?.message}
+      <Field.Input label="E-mail" name="email" ref={register} />
+      {errors.email?.message}
       <Button>Submit</Button>
     </Form>
   )
